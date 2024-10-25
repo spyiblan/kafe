b5f30439b334c61db8a1e4e36f2adfcd6b760826
spyiblan <muhdfadhlan2005@gmail.com>
2024-10-25 10:31:29 +0800

    upload

--- set 2 sesi 2 2024/kafe/kafe/settings.py
+++ set 2 sesi 2 2024/kafe/kafe/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,3 +126,5 @@ STATIC_ROOT=BASE_DIR/'staticfiles'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
