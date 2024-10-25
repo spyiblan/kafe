dbd766712a388c654a8ac4724aa5463d85dc7d8b
spyiblan <muhdfadhlan2005@gmail.com>
2024-10-25 11:06:20 +0800

    upload

--- set 2 sesi 2 2024/kafe/kafe/settings.py
+++ set 2 sesi 2 2024/kafe/kafe/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.miidleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'kafe.urls'
