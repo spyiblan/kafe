09b1381b4cdbce4846bcb6dd31dec2f076dc815c
spyiblan <muhdfadhlan2005@gmail.com>
2024-10-25 10:13:27 +0800

    upload

--- set 2 sesi 2 2024/kafe/kafe/settings.py
+++ set 2 sesi 2 2024/kafe/kafe/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR/'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
